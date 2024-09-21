368667824baf8a72d28970325d59d305b5329820
Guilherme Lunghini Teixeira <161391446+glunghini@users.noreply.github.com>
2024-09-20 22:42:54 -0300

    sexto porra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import CardContainer from "../components/CardContainer"
+import MovieCard from "../components/MovieCard"
+import movies from "../data/movies.json"
+
 export default function Home(){
     return(
-        <h1> Home Page</h1>
+        <>
+        <CardContainer titulo = 'Filmes antigos'>
+            {
+                movies
+                .filter( filme => (filme.ano_lancamento < 2008))
+                .map( filme => (
+                    <MovieCard key={filme.id} {...filme}/>
+                ))
+            }
+        </CardContainer>
+        <CardContainer titulo = 'Melhor Avaliadps'>
+            {
+                movies
+                .filter( filme => (filme.avaliacao > 9))
+                .map( filme => (
+                    <MovieCard key={filme.id} {...filme}/>
+                ))
+            }
+        </CardContainer>
+        
+        </>
     )
 }
