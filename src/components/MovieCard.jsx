fe972b57dffb5a1c93bed3444f38c644a41a94fa
Guilherme Lunghini Teixeira <161391446+glunghini@users.noreply.github.com>
2024-09-27 22:43:28 -0300

    xbcxfnbfbd

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ export default function MovieCard({id,titulo,imagem_destaque}) {
     return(
         <div>
             <h2>{titulo}</h2>
-            <img src={imagem_destaque}/>
+            <img className = 'h-20' src={imagem_destaque}/>
             <Link to={`/movies/${id}`}> Saber mais</Link>
         </div>
     )
