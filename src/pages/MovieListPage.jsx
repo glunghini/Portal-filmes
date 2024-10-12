fb16171274cee0ca2d3739e37eda936e80f9c0a7
Guilherme Lunghini Teixeira <161391446+glunghini@users.noreply.github.com>
2024-10-11 22:37:45 -0300

    receba

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 import movies from "../data/movies.json"
 
 export default function MovieListPage() {
 
     const [search, setSearch] = useState("")
+    const [filmes, setFilmes] = useState([]) // Inicializando como um array vazio
 
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br')
+            .then(data => data.json())
+            .then(res => setFilmes(res.results))
+            .catch(erro => console.log(erro))
+            .finally(() => console.log('Feito!'))
+    }, [])
+    
     const handleSearch = (event) => {
         setSearch(event.target.value)
         console.log(search)
     }
 
-    const filmesFiltrados = movies.filter(filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+    const filmesFiltrados = filmes.filter(filme => filme.title.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <>
@@ -23,19 +32,19 @@ export default function MovieListPage() {
                 value={search}
                 onChange={handleSearch}
             />
-            <section className="flex">
+            <section className="">
                 {
-                    filmesFiltrados.length > 0 ?
-
-                        filmesFiltrados
-                            .map(filme => (
-                                <MovieCard key={filme.id} {...filme} />
-                            ))
-                        :
-                        <p> Filme não encontrado</p>
+                    filmesFiltrados.map(filme => 
+                        (
+                            <div key={filme.id}> {/* Adicione uma chave única para cada item */}
+                                <h1>{filme.title}</h1>
+                                <p>{filme.vote_average}</p>
+                                <img src={`https://image.tmdb.org/t/p/w1280${filme.backdrop_path}`} alt={filme.title} />
+                                <img src={`https://image.tmdb.org/t/p/w185${filme.poster_path}`}/>
+                            </div>
+                        ))
                 }
             </section>
         </>
     )
 }
-
